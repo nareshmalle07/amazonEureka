868535d8bb99f17ba9a4174d0aeed833c458b149
ruchi <yalakantisairuchitha@gmail.com>
2026-08-11 17:41:18 +0530

    first commit

---
+++ amazonEureka/src/main/java/com/amazon/amazonEureka/AmazonEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.amazon.amazonEureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonEurekaApplication.class, args);
+	}
+
+}
